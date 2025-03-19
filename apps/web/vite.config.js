c9d8c69ba6f807728dc72f8c016a2944035692b6
Deadwalk <zgdmemail@gmail.com>
2025-03-19 20:40:02 +0800

    修复ws链接错误的问题

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -42,14 +42,7 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
         ws: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('代理错误', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('代理请求', req.method, req.url);
-          });
-        }
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       },
       '/ws': {
         target: 'ws://localhost:8000',
